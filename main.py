3007a3b2d859b7811df8f3146fa4111307e21b1e
alex <you@example.com>
2023-10-18 10:10:20 +0500

    deleted wrong spaces

--- main.py
+++ main.py
@@ -9,83 +9,82 @@ import win32serviceutil
 import servicemanager
 import win32event
 import win32service
- 
+
 TIMEOUT = 10
- 
+
 class SMWinservice(win32serviceutil.ServiceFramework):
-    """Base class to create winservice in Python"""
-    _svc_name_ = 'control_time'
-    _svc_display_name_ = 'Control time'
-    _svc_description_ = 'Control time'
- 
-    @classmethod
-    def parse_command_line(cls):
-        """ClassMethod to parse the command line"""
-        win32serviceutil.HandleCommandLine(cls)
- 
-    def __init__(self, args):
-        """Constructor of the winservice"""
-        win32serviceutil.ServiceFramework.__init__(self, args)
-        self.hWaitStop = win32event.CreateEvent(None, 0, 0, None)
-        socket.setdefaulttimeout(60)
- 
-    def SvcStop(self):
-        """Called when the service is asked to stop"""
-        self.stop()
-        self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
-        win32event.SetEvent(self.hWaitStop)
- 
-    def SvcDoRun(self):
-        """Called when the service is asked to start"""
-        self.start()
-        servicemanager.LogMsg(servicemanager.EVENTLOG_INFORMATION_TYPE,
-                              servicemanager.PYS_SERVICE_STARTED,
-                              (self._svc_name_, ''))
-        self.main()
- 
-    def start(self):
-        """
-        Override to add logic before the start
-        eg. running condition
-        """
-        pass
- 
-    def stop(self):
-        """
-        Override to add logic before the stop
-        eg. invalidating running condition
-        """
-        pass
- 
-    def main(self):
-        """Main class to be ovverridden to add logic"""
-        pass
- 
+    """Base class to create winservice in Python"""
+    _svc_name_ = 'control_time'
+    _svc_display_name_ = 'Control time'
+    _svc_description_ = 'Control time'
+    @classmethod
+    def parse_command_line(cls):
+        """ClassMethod to parse the command line"""
+        win32serviceutil.HandleCommandLine(cls)
+
+    def __init__(self, args):
+        """Constructor of the winservice"""
+        win32serviceutil.ServiceFramework.__init__(self, args)
+        self.hWaitStop = win32event.CreateEvent(None, 0, 0, None)
+        socket.setdefaulttimeout(60)
+
+    def SvcStop(self):
+        """Called when the service is asked to stop"""
+        self.stop()
+        self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
+        win32event.SetEvent(self.hWaitStop)
+
+    def SvcDoRun(self):
+        """Called when the service is asked to start"""
+        self.start()
+        servicemanager.LogMsg(servicemanager.EVENTLOG_INFORMATION_TYPE,
+                              servicemanager.PYS_SERVICE_STARTED,
+                              (self._svc_name_, ''))
+        self.main()
+
+    def start(self):
+        """
+        Override to add logic before the start
+        eg. running condition
+        """
+        pass
+
+    def stop(self):
+        """
+        Override to add logic before the stop
+        eg. invalidating running condition
+        """
+        pass
+
+    def main(self):
+        """Main class to be ovverridden to add logic"""
+        pass
+
 class PythonCornerExample(SMWinservice):
- 
-    def start(self):
-        self.isrunning = True
- 
-    def stop(self):
-        self.isrunning = False
- 
-    def main(self):
-        while self.isrunning:
-            self.control_time()
-            time.sleep(50)
- 
-    def control_time(self):
-        """Проверяем текущего пользователя и текущее время"""
-        current_user = os.getlogin()
-        target_time_delta : bool = 23 <= datetime.now().hour < 6
-        if 'dasha' in current_user.lower() and target_time_delta:
-            toast(f'The computer will turn off in {TIMEOUT} seconds')
-            time.sleep(TIMEOUT)
-            os.system('shutdown -s')
- 
+
+    def start(self):
+        self.isrunning = True
+
+    def stop(self):
+        self.isrunning = False
+
+    def main(self):
+        while self.isrunning:
+            self.control_time()
+            time.sleep(50)
+
+    def control_time(self):
+        """Проверяем текущего пользователя и текущее время"""
+        current_user = os.getlogin()
+        target_time_delta : bool = 23 <= datetime.now().hour < 6
+        if 'dasha' in current_user.lower() and target_time_delta:
+            toast(f'The computer will turn off in {TIMEOUT} seconds')
+            time.sleep(TIMEOUT)
+            os.system('shutdown -s')
+
 if __name__ == '__main__':
-    PythonCornerExample.parse_command_line()
- 
+    PythonCornerExample.parse_command_line()
+
 # if __name__ == '__main__':
 #     # toast('Hello Python')
 #     toast(f'The computer will turn off in {TIMEOUT} seconds')
